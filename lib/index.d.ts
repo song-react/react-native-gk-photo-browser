192019657ab0ce4bf5421db5f01364c83d38d7e9
song <>
2026-05-09 22:55:26 +0800

    build: update release types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,13 +1,20 @@
 import type { BrowserConfig as NativeBrowserConfig, GKPhotoBrowser } from './specs/GKPhotoBrowser.nitro';
 export type { BrowserCallbacks, BrowserImage, BrowserMediaType, BrowserRect, GKPhotoBrowser } from './specs/GKPhotoBrowser.nitro';
-export type BrowserShowStyle = 'none' | 'zoom' | 'push';
-export type BrowserHideStyle = 'zoom' | 'zoomScale' | 'zoomSlide';
-export type BrowserLoadStyle = 'indeterminate' | 'indeterminateMask' | 'determinate' | 'custom';
-export type BrowserConfig = Omit<NativeBrowserConfig, 'showStyle' | 'hideStyle' | 'loadStyle' | 'originLoadStyle'> & {
+export type BrowserShowStyle = 'none' | 'zoom' | 'push' | 'pushZoom';
+export type BrowserHideStyle = 'none' | 'zoom' | 'zoomScale' | 'zoomSlide';
+export type BrowserLoadStyle = 'indeterminate' | 'indeterminateMask' | 'determinate' | 'determinateSector' | 'custom';
+export type BrowserFailStyle = 'onlyText' | 'onlyImage' | 'imageAndText' | 'custom';
+export type BrowserStatusBarStyle = 'default' | 'light' | 'dark';
+export type BrowserConfig = Omit<NativeBrowserConfig, 'showStyle' | 'hideStyle' | 'loadStyle' | 'originLoadStyle' | 'videoLoadStyle' | 'liveLoadStyle' | 'failStyle' | 'videoFailStyle' | 'statusBarStyle'> & {
     showStyle?: BrowserShowStyle;
     hideStyle?: BrowserHideStyle;
     loadStyle?: BrowserLoadStyle;
     originLoadStyle?: BrowserLoadStyle;
+    videoLoadStyle?: BrowserLoadStyle;
+    liveLoadStyle?: BrowserLoadStyle;
+    failStyle?: BrowserFailStyle;
+    videoFailStyle?: BrowserFailStyle;
+    statusBarStyle?: BrowserStatusBarStyle;
 };
 export declare const PhotoBrowser: GKPhotoBrowser;
 //# sourceMappingURL=index.d.ts.map
